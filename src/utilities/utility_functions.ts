928a6a635d61d16dafd6ecda7b7d84db518b2e8f
Selena Groh <selena.groh@gmail.com>
2024-10-01 22:41:43 -0400

    add types

--- src/utilities/utility_functions.ts
+++ src/utilities/utility_functions.ts
@@ -1,7 +1,7 @@
 // Shuffles and returns a shallow copy of the array using the Fisher-Yates algorithm
-export function shuffleArray(oldArray: any[]): any[] {
-  var j, x, i;
-  var newArray = oldArray;
+export function shuffleArray<T>(oldArray: T[]): T[] {
+  let j, x, i;
+  const newArray = oldArray;
   for (i = newArray.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
     x = newArray[i];
@@ -12,9 +12,18 @@ export function shuffleArray(oldArray: any[]): any[] {
 }
 
 // Converts [Color.White, Color.Blue, etc...] into an object like {[Color.White]: 0, [Color.Blue]: 1, etc...}
-export function arrayToValueKeyObject(arr) {
-  return arr.reduce((acc, value, index) => {
-    acc[value] = index;
-    return acc;
-  }, {});
+export function arrayToValueKeyObject<T extends string | number | symbol>(
+  arr: T[]
+): {
+  [key in T]?: number;
+} {
+  return arr.reduce(
+    (acc, value, index) => {
+      acc[value] = index;
+      return acc;
+    },
+    {} as {
+      [key in T]?: number;
+    }
+  );
 }
