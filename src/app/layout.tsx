f15bc39c9dc90aabb0a3e18b0854d0c82baa39f6
Selena Groh <selena.groh@gmail.com>
2024-11-27 14:41:49 -0500

    Update analytics import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { Providers } from "./providers";
 import { fonts } from "./fonts";
 
